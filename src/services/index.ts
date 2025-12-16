f1daf9d859a9344594732ec9baa5cb21b58ab1fc
赵亚祥 <1358293899@qq.com>
2025-12-16 10:43:48 +0800

    修改接口域名

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 import type { Question } from './type'
 
 export const getData = () => {
-  return axios.get<Question[]>('http://39.96.210.90:3000/api/exam_questions')
+  return axios.get<Question[]>('https://zyxcl.xyz/data/api/exam_questions')
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +12,23 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
+  },
+  base: '/exam',
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
   }
 })
